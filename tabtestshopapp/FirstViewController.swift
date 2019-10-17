ec61067e5639b3ef74d287aa9d94a5acdd17386d
shiuhyaw <psy5851@gmail.com>
2019-10-17 10:59:28 +0800

    Initial Commit

---
+++ tabtestshopapp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabtestshopapp
+//
+//  Created by Shiuh Yaw Phang on 17/10/19.
+//  Copyright © 2019 Shiuh Yaw Phang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tabtestshopapp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tabtestshopapp
+//
+//  Created by Shiuh Yaw Phang on 17/10/19.
+//  Copyright © 2019 Shiuh Yaw Phang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
